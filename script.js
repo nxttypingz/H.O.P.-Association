373dc8ec32738fca8cee80f7105583cc1d8f9563
Natty <92677089+nxttypingz@users.noreply.github.com>
2021-10-23 08:57:58 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,57 @@
+var count= 0;
+
+var count2= 0;
+
+const titleList= [["Consideration" , Consideration is having kindness and
+a thoughtful regard for others or an act of thoughtfulness. Treat others
+with the same feeling you would want to be treated with. Having such a golden
+rule is  sometimes forgotten and only remembered when looked back at or
+reminded of. With having just a bit of consideration cause a great deal for
+looking at others in a lower level than us.] ["Assumstions" , When one hears
+about another suffering we automatically think of something major. 'It must be
+the death of a loved one' or 'Is it another natural disatster this place got
+struck by?' The meaning of suffer in fact means to experience or be subjected
+to something bad or unpleasant. So suffering does not only mean a job lost or
+a divorce, it also means going through something mentally disturbing or even
+having overall negative emotions because of a certain situation.]
+["Your Help Can Pay" , In the world, there are so many people we see who are
+suffering; with the naked eye, we see a homeless person but sometimes that one
+joyful co-worker or that kid at school who is the 'loner' of the class is suffering
+as well, so no matter who you see think twice about that positive action you want
+to do to help them.] ["Treasuring Your Actions And Not Just Value Them" , There
+are some people who will help, they will contribute, but their intentions are
+different. Some may do it for fame or recognition others may do it out of boredom
+but you can never know what one's true intentions are.]]
+let [title, information] = titleList[count]
+
+displayThis();
+
+var myTitle= document.getElementById("consideration");
+
+var myInformation= document.getElementById("consideration");
+
+var myTitle= document.getElementById("only-assumstions");
+
+var myInformation= document.getElementById("only-assumstions");
+
+var myTitle= document.getElementById("help");
+
+var myInformation= document.getElementById("help");
+
+var myTitle= document.getElementById("hashtag");
+
+var myInformation= document.getElementById("hashtag");
+
+var myquestion= document.getElementById("myquestion");
+
+
+
+myTitle.addEventListener("click", displayExplainf);
+
+myInformation.addEventListener("click", displayExplainm);
+
+
+function displayExplainf() {
+ let [title, information] = titleList[count]
+ 
+}
